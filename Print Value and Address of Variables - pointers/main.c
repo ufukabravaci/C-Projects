54d3c2787328b484934faf8e2ef48eb199d31e4b
ufukabravaci <ufkk1262@gmail.com>
2022-07-14 02:06:54 +0300

    Pointers

---
+++ Print Value and Address of Variables - pointers/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int grade1=60 , grade2=95 ;
+
+    printf("Grade1 value = %d\n", grade1);
+    printf("Grade1 adress = %p\n", &grade1);
+    printf("Grade2 value = %d\n", grade2);
+    printf("Grade2 adress = %p\n", &grade2);
+
+    return 0;
+}
